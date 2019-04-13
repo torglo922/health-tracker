60fac98ff4d3dd6a52cd139b78efa0143ddd5059
Emil Naydyonov <enaydyonov96@inbox.ru>
2019-04-13 18:26:47 +0300

    get information from jwts

--- graphql-api/src/index.ts
+++ graphql-api/src/index.ts
@@ -1,8 +1,15 @@
 import { bootstrap } from "vesper";
 import { GraphQLDate, GraphQLDateTime, GraphQLTime } from "graphql-iso-date";
+import * as OktaJwtVerifier from '@okta/jwt-verifier';
+import { CurrentUser } from './CurrentUser';
 import { PointsController } from "./controller/PointsController";
 import { Points } from "./entity/Points";
 import { User } from "./entity/User";
+
+const oktaJwtVerifier = new OktaJwtVerifier({
+  clientId: '{yourClientId}',
+  issuer: 'https://{yourOktaDomain}.com/oauth2/default'
+});
 
 bootstrap({
   port: 4000,
@@ -10,6 +17,24 @@ bootstrap({
   entities: [Points, User],
   schemas: [__dirname + "/schema/**/*.graphql"],
   cors: true,
+  setupContainer: async (container, action) => {
+    const request = action.request;
+    // require every request to have an authorization header
+    if (!request.headers.authorization) {
+      throw Error('Authorization header is required!');
+    }
+    let parts = request.headers.authorization.trim().split(' ');
+    let accessToken = parts.pop();
+    await oktaJwtVerifier.verifyAccessToken(accessToken)
+      .then(async jwt => {
+        const user = JSON.parse(request.headers['x-forwarded-user'].toString());
+        const currentUser = new CurrentUser(jwt.claims.uid, user.given_name, user.family_name);
+        container.set(CurrentUser, currentUser);
+      })
+      .catch(error => {
+        throw Error('JWT Validation failed!');
+      })
+  },
   customResolvers: {
     Date: GraphQLDate,
     Time: GraphQLTime,
